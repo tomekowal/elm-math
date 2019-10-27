3a992dad6c3da5ea044ddbdd308b00016aec4158
Tomasz Kowal <tomasz.kowal@clubcollect.com>
2019-10-27 11:55:12 +0100

    Add divisions and subtractions

--- src/Puzzle.elm
+++ src/Puzzle.elm
@@ -5,7 +5,9 @@ import Random
 
 type Puzzle
     = Multiplication Int Int
+    | Division Int Int
     | Addition Int Int
+    | Subtraction Int Int
 
 
 initial : Puzzle
@@ -13,24 +15,39 @@ initial =
     Multiplication 0 0
 
 
-digitGenerator : Random.Generator Int
-digitGenerator =
+zeroToTen : Random.Generator Int
+zeroToTen =
     Random.int 0 10
+
+
+oneToTen : Random.Generator Int
+oneToTen =
+    Random.int 1 10
 
 
 multGenerator : Random.Generator Puzzle
 multGenerator =
-    Random.map2 Multiplication digitGenerator digitGenerator
+    Random.map2 Multiplication zeroToTen zeroToTen
+
+
+divGenerator : Random.Generator Puzzle
+divGenerator =
+    Random.map2 Division oneToTen zeroToTen
 
 
 addGenerator : Random.Generator Puzzle
 addGenerator =
-    Random.map2 Addition digitGenerator digitGenerator
+    Random.map2 Addition zeroToTen zeroToTen
+
+
+subGenerator : Random.Generator Puzzle
+subGenerator =
+    Random.map2 Subtraction zeroToTen zeroToTen
 
 
 puzzleGenerator : Random.Generator Puzzle
 puzzleGenerator =
-    Random.int 1 2
+    Random.int 1 4
         |> Random.andThen intToPuzzle
 
 
@@ -40,8 +57,14 @@ intToPuzzle int =
         1 ->
             multGenerator
 
-        _ ->
+        2 ->
+            divGenerator
+
+        3 ->
             addGenerator
+
+        _ ->
+            subGenerator
 
 
 isCorrect : Puzzle -> Int -> Bool
@@ -50,8 +73,14 @@ isCorrect puzzle guess =
         Multiplication left right ->
             left * right == guess
 
+        Division left right ->
+            right == guess
+
         Addition left right ->
             left + right == guess
+
+        Subtraction left right ->
+            right == guess
 
 
 puzzleToString : Puzzle -> String
@@ -60,5 +89,11 @@ puzzleToString puzzle =
         Multiplication left right ->
             String.fromInt left ++ " x " ++ String.fromInt right
 
+        Division left right ->
+            String.fromInt (left * right) ++ " / " ++ String.fromInt left
+
         Addition left right ->
             String.fromInt left ++ " + " ++ String.fromInt right
+
+        Subtraction left right ->
+            String.fromInt (left + right) ++ " - " ++ String.fromInt left

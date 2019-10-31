0e541a4ef916e2a87b583ea772005b08d93f69fe
Tomasz Kowal <tomasz.kowal@clubcollect.com>
2019-11-01 00:10:43 +0100

    Move Msg to top of update; add with* functions; narrow function signatures

--- src/Main.elm
+++ src/Main.elm
@@ -32,6 +32,15 @@ main =
 -- MODEL
 
 
+type alias Model =
+    { guess : Guess
+    , time : Time
+    , puzzle : Puzzle.Puzzle
+    , score : Int
+    , settings : Settings
+    }
+
+
 type alias Guess =
     Maybe Int
 
@@ -44,20 +53,11 @@ type alias Settings =
     { addition : Bool }
 
 
-type alias Game =
-    { guess : Guess
-    , time : Time
-    , puzzle : Puzzle.Puzzle
-    , score : Int
-    , settings : Settings
-    }
-
-
-init : () -> ( Game, Cmd Msg )
+init : () -> ( Model, Cmd Msg )
 init _ =
     let
-        game =
-            Game Nothing 20 Puzzle.initial 0 initialSettings
+        model =
+            Model Nothing 20 Puzzle.initial 0 initialSettings
 
         generator =
             Random.generate NewPuzzle Puzzle.puzzleGenerator
@@ -65,7 +65,7 @@ init _ =
         focusEvent =
             Task.attempt (\_ -> NoOp) (Browser.Dom.focus "guess")
     in
-    ( game, Cmd.batch [ generator, focusEvent ] )
+    ( model, Cmd.batch [ generator, focusEvent ] )
 
 
 initialSettings : Settings
@@ -90,67 +90,80 @@ type Msg
     | NoOp
 
 
-update : Msg -> Game -> ( Game, Cmd Msg )
-update msg game =
-    case game.time of
-        0 ->
-            case msg of
-                Tick _ ->
-                    ( game, Cmd.none )
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Tick _ ->
+            case model.time of
+                0 ->
+                    ( model, Cmd.none )
 
-                NewPuzzle _ ->
-                    ( game, Cmd.none )
+                time ->
+                    ( decreaseTime model, Cmd.none )
 
-                Guess _ ->
-                    ( game, Cmd.none )
+        NewPuzzle puzzle ->
+            case model.time of
+                0 ->
+                    ( model, Cmd.none )
 
-                Restart ->
+                _ ->
+                    ( model |> withPuzzle puzzle, Cmd.none )
+
+        Guess stringGuess ->
+            handleGuess stringGuess model
+
+        Restart ->
+            case model.time of
+                0 ->
                     init ()
 
-                Setting settingMsg ->
-                    updateGameSettings settingMsg game
+                _ ->
+                    ( model, Cmd.none )
 
-                NoOp ->
-                    ( game, Cmd.none )
+        Setting settingMsg ->
+            ( updateModelSettings settingMsg model, Cmd.none )
 
-        time ->
-            case msg of
-                Guess stringGuess ->
-                    let
-                        guess =
-                            String.toInt stringGuess
-                    in
-                    if isCorrect game.puzzle guess then
-                        ( { game | time = game.time + 1, guess = Nothing, score = game.score + 1 }
-                        , Random.generate NewPuzzle Puzzle.puzzleGenerator
-                        )
-
-                    else
-                        ( { game | guess = guess }, Cmd.none )
-
-                Restart ->
-                    ( game, Cmd.none )
-
-                Tick _ ->
-                    ( { game | time = time - 1 }, Cmd.none )
-
-                NewPuzzle puzzle ->
-                    ( { game | puzzle = puzzle }, Cmd.none )
-
-                Setting settingMsg ->
-                    updateGameSettings settingMsg game
-
-                NoOp ->
-                    ( game, Cmd.none )
+        NoOp ->
+            ( model, Cmd.none )
 
 
-updateGameSettings : SettingMsg -> Game -> ( Game, Cmd Msg )
-updateGameSettings settingMsg game =
+decreaseTime : Model -> Model
+decreaseTime model =
+    { model | time = model.time - 1 }
+
+
+increaseTime : Model -> Model
+increaseTime model =
+    { model | time = model.time + 1 }
+
+
+clearGuess : Model -> Model
+clearGuess model =
+    { model | guess = Nothing }
+
+
+withGuess : Guess -> Model -> Model
+withGuess guess model =
+    { model | guess = guess }
+
+
+withPuzzle : Puzzle.Puzzle -> Model -> Model
+withPuzzle puzzle model =
+    { model | puzzle = puzzle }
+
+
+increaseScore : Model -> Model
+increaseScore model =
+    { model | score = model.score + 1 }
+
+
+updateModelSettings : SettingMsg -> Model -> Model
+updateModelSettings settingMsg model =
     let
         newSettings =
-            updateSettings settingMsg game.settings
+            updateSettings settingMsg model.settings
     in
-    ( { game | settings = newSettings }, Cmd.none )
+    { model | settings = newSettings }
 
 
 updateSettings : SettingMsg -> Settings -> Settings
@@ -160,13 +173,36 @@ updateSettings settingMsg settings =
             { settings | addition = bool }
 
 
+handleGuess : String -> Model -> ( Model, Cmd Msg )
+handleGuess stringGuess model =
+    case model.time of
+        0 ->
+            ( model, Cmd.none )
+
+        _ ->
+            let
+                guess =
+                    String.toInt stringGuess
+            in
+            if isCorrect model.puzzle guess then
+                ( model
+                    |> increaseScore
+                    |> increaseTime
+                    |> clearGuess
+                , Random.generate NewPuzzle Puzzle.puzzleGenerator
+                )
+
+            else
+                ( model |> withGuess guess, Cmd.none )
+
+
 
 -- SUBSCRIPTIONS
 
 
-subscriptions : Game -> Sub Msg
-subscriptions game =
-    case game.time of
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    case model.time of
         0 ->
             Sub.none
 
@@ -178,39 +214,39 @@ subscriptions game =
 -- VIEW
 
 
-view : Game -> Html Msg
-view game =
-    Element.layout [] (container game)
+view : Model -> Html Msg
+view model =
+    Element.layout [] (container model)
 
 
-container : Game -> Element Msg
-container game =
+container : Model -> Element Msg
+container model =
     column [ spacing 10, centerX ]
-        [ timer game
-        , el defaultStyle <| Element.text <| "Score: " ++ String.fromInt game.score
-        , el defaultStyle <| Element.text <| Puzzle.puzzleToString game.puzzle
+        [ timer model.time
+        , el defaultStyle <| Element.text <| "Score: " ++ String.fromInt model.score
+        , el defaultStyle <| Element.text <| Puzzle.puzzleToString model.puzzle
         , Input.text (Input.focusedOnLoad :: htmlAttribute numericKeyboardAttr :: htmlAttribute inputId :: defaultStyle)
-            { text = guessToString game.guess
+            { text = guessToString model.guess
             , onChange = Guess
             , placeholder = Nothing
             , label = Input.labelHidden "Guess"
             }
-        , maybeTryAgainButton game
+        , maybeTryAgainButton model.time
         ]
 
 
 
--- I want the same height of Timer Bar as Game Over text,
+-- I want the same height of Timer Bar as Model Over text,
 -- so I insert space in empty timer
 
 
-timer : Game -> Element Msg
-timer game =
-    case game.time of
+timer : Time -> Element Msg
+timer time =
+    case time of
         0 ->
             el defaultStyle <| Element.text "Game Over!"
 
-        time ->
+        _ ->
             el
                 ([ width <| px <| timerWidth time
                  , Background.color primaryColor
@@ -225,26 +261,26 @@ timerWidth x =
     x * 20
 
 
-maybeTryAgainButton : Game -> Element Msg
-maybeTryAgainButton game =
-    case game.time of
+maybeTryAgainButton : Time -> Element Msg
+maybeTryAgainButton time =
+    case time of
         0 ->
             Input.button defaultStyle
                 { label = Element.text "Try again!"
                 , onPress = Just Restart
                 }
 
-        time ->
+        _ ->
             el defaultStyle <| Element.text " "
 
 
-settingsRow : Game -> Element Msg
-settingsRow game =
+settingsRow : Settings -> Element Msg
+settingsRow settings =
     row defaultStyle
         [ Input.checkbox []
             { onChange = \bool -> Setting <| Addition bool
             , icon = Input.defaultCheckbox
-            , checked = game.settings.addition
+            , checked = settings.addition
             , label =
                 Input.labelBelow []
                     (text "+")
